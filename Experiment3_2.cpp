288b8018e177fa04f43153921a340805fada1153
mickoedep <47804197+mickoedep@users.noreply.github.com>
2019-05-21 13:49:55 +0800

    Add files via upload

---
+++ Experiment3_2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int p = 3;
+int d = 7;
+int y;
+
+int main()
+
+{
+	int temp[p][d];
+
+	cout << "Enter all temperature for a week of Province A, Province B and then Province C.\n" << endl;
+	{
+		for (y = 0; y < d; y++)
+		{
+			cout << "Province A, Day " << y + 1 << " : ";
+			cin >> temp[0][y];
+		}
+
+		for (y = 0; y < d; y++)
+		{
+			cout << "Province B, Day " << y + 1 << " : ";
+			cin >> temp[1][y];
+		}
+		for (y = 0; y < d; y++)
+		{
+
+			cout << "Province C, Day " << y + 1 << " : ";
+			cin >> temp[2][y];
+		}
+	}
+		cout << "\nDisplaying Values : \n" << endl;
+	{
+		for (y= 0; y< d; y++)
+		{
+			cout << "Province A, Day " << y + 1 << " = " << temp[0][y] << endl; 
+
+		}
+
+		for (y = 0; y < d; y++)
+		{
+			cout << "Province B, Day " << y + 1 << " = " << temp[1][y] << endl;
+		}
+		for (y = 0; y < d; y++)
+
+		{
+			cout << "Province C, Day " << y + 1 << " = " << temp[2][y] << endl;
+		}
+
+	}
+	_getch();
+	return 0;
+}
+
+
+
---
+++ Experiment3_3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<conio.h>
+#include <cstring>
+
+using namespace std;
+int main()
+{
+	char a[500];
+	int s;
+
+	cout <<"Enter characters: ";
+	cin.getline(a,500);
+	s=strlen(a);
+	for (int i = s; i >= 0; i--)
+	{
+		cout <<a[i];
+	}
+	cout << "\nArray Size:" << s+1 << endl;
+
+ _getch();
+return 0;
+}
+
